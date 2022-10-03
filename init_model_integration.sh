9b683a1577a2e328eb2d54032710a7d3fd755261
oct1971 <oct@big.or.jp>
2022-10-04 01:18:34 +0900

    modelファイルの置き場所変更に対応

--- init_model_integration.sh
+++ init_model_integration.sh
@@ -3,12 +3,12 @@ mkdir data_dir
 mkdir outputs
 mkdir log
 cd data_dir
-mkdir ESRGAN
 mkdir models
+mkdir models/ESRGAN
+mkdir models/SwinIR
 mkdir realesrgan-weight
 mkdir weights-codeformer
 mkdir weights-facelib
 mkdir weights
-mkdir SwinIR
 echo '{}' > config.json
 echo '{}' > ui-config.json
